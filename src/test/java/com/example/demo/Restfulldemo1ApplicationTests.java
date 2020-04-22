d7287cacba3c98988e734c8da8c11f6d2e1b66b8
sw1988311 <sw1988311@126.com>
2020-04-22 22:58:24 +0800

    get data from properties

--- src/test/java/com/example/demo/Restfulldemo1ApplicationTests.java
+++ src/test/java/com/example/demo/Restfulldemo1ApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import com.example.demo.pojo.CoExample;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Restfulldemo1ApplicationTests {
 
+	@Autowired
+	private CoExample coExample;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void get_age(){
+		System.out.println(coExample.getAge());
+	}
+
+	@Test
+	public void get_name(){
+		System.out.println(coExample.getName());
+	}
+
+	@Test
+	public void get_address(){
+		System.out.println(coExample.getAddress());
 	}
 
 }
